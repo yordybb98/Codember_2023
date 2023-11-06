b716078a95e116e5f71e4b9c55b21b36e7f22a87
N1GH7M4R3 <yordanbb98@gmail.com>
2023-11-05 22:05:04 -0500

    Challenge01

--- challenge01/index.js
+++ challenge01/index.js
@@ -1,1 +1,40 @@
-console.log("Hola");
+function countItemInArray(item, array){
+
+    const total = array.reduce((count, element)=> {
+        if(element === item) count ++
+        return count;
+    }, 0)
+
+    return total
+}
+
+async function challenge01() {
+    try {
+        const response = await fetch("https://codember.dev/data/message_01.txt");
+        
+        if (!response.ok) {
+            throw new Error(`Request error: ${response.status}`);
+        }
+
+        const message = await response.text();
+
+        // Splitting the words
+        const words = message.toLocaleLowerCase().split(" ");
+        
+        //Removing Duplicates Keys
+        uniqueSet = new Set(Object.values(words))
+        uniqueWords = Array.from(uniqueSet)
+
+        //Concatening every unique element with its counts
+        let result = "";
+        uniqueWords.forEach(element => {
+            result = result.concat(element + countItemInArray(element, words)); 
+        });
+        console.log(result);
+    } catch (error) {
+        console.error("An error ocurrs when executing the function challenge01:", error);
+    }
+}
+
+challenge01();
+
